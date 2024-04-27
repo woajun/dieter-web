ed5970b6da06aeb362ff6231e678420493694a5f
woajun <woajun@naver.com>
2024-04-27 23:56:24 +0900

    feat: 네비게이션

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,32 +1,19 @@
-import styles from "./Navigation.module.scss";
-import { useLocation } from "react-router-dom";
-import { CSSProperties, ReactNode } from "react";
-import { Hamburger } from "./Hamburger";
-import { DownSvg } from "../../assets/svgs/DownSvg";
-import { KoreaSvg } from "../../assets/svgs/KoreaSvg";
-import { AmericaSvg } from "../../assets/svgs/AmericaSvg";
-import { JLink } from "../JLink";
-
-const NavItem = ({
-  children,
-  to,
-  intercept,
-}: {
-  children: ReactNode;
-  to: string;
-  intercept?: string;
-}) => {
-  const { pathname } = useLocation();
-  const isActive = pathname.startsWith(to);
-  return (
-    <JLink
-      className={isActive ? styles.activeLink : styles.menuLink}
-      to={intercept ?? to}
-    >
-      {children}
-    </JLink>
-  );
-};
+import {
+  AppBar,
+  Avatar,
+  Box,
+  Button,
+  Container,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import { CSSProperties, useState } from "react";
+import AdbIcon from "@mui/icons-material/Adb";
+import MenuIcon from "@mui/icons-material/Menu";
 
 export type MenuItem = {
   text: string;
@@ -42,81 +29,149 @@ type Props = {
   style?: CSSProperties;
 };
 
+const pages = ["Products", "Pricing", "Blog"];
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
+
 export const Navigation = ({ style }: Props) => {
-  const menuItems: MenuItem[] = [
-    { text: "About", to: "/about" },
-    { text: "Service", to: "/service" },
-    {
-      text: "Board",
-      to: "/board",
-      intercept: "/board/press",
-      subMenu: [
-        { text: "boardPress", to: "/board/press" },
-        { text: "boardNews", to: "/board/news" },
-      ],
-    },
-    {
-      text: "Contact",
-      to: "/contact",
-      intercept: "/contact/location",
-      subMenu: [
-        // { text: 'inquiry', to: '/contact/inquiry' },
-        { text: "location", to: "/contact/location" },
-        { text: "recruit", to: "/contact/recruit" },
-      ],
-    },
-  ];
+  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+  const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
   return (
-    <nav className={styles.navigation} style={style}>
-      <div className={styles.navigationWrapper}>
-        <Hamburger menuItems={menuItems} />
+    <AppBar position="static" style={style}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            DIETER
+          </Typography>
 
-        <div className={styles.logo}>
-          <JLink to="/"></JLink>
-        </div>
-
-        <div className={styles.menu}>
-          <ul className={styles.menuUl}>
-            {menuItems.map((item, index) => (
-              <li key={index} className={styles.menuLi}>
-                <NavItem to={item.to} intercept={item.intercept}>
-                  {item.text}
-                </NavItem>
-                {item.subMenu && (
-                  <ul className={styles.subMenu}>
-                    {item.subMenu.map((subItem, subIndex) => (
-                      <li key={subIndex}>
-                        <NavItem to={subItem.to}>{subItem.text}</NavItem>
-                      </li>
-                    ))}
-                  </ul>
-                )}
-              </li>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            LOGO
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
+                {page}
+              </Button>
             ))}
-          </ul>
-        </div>
+          </Box>
 
-        <div className={styles.language}>
-          <div className={styles.languageButton}>
-            <>
-              <KoreaSvg />
-              <span>Korean</span>
-            </>
-            <DownSvg />
-            <ul className={styles.subLanguageUl}>
-              <li onClick={() => {}}>
-                <KoreaSvg />
-                Korean
-              </li>
-              <li onClick={() => {}}>
-                <AmericaSvg />
-                English
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </nav>
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 };
